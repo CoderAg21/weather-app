9d366e9a818ab930869a520497e05e8c19b6c534
CoderAg21 <84915319+CoderAg21@users.noreply.github.com>
2024-06-29 12:12:14 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//global scope
 let searchArea = document.getElementById("searchArea");
 let searchContent = document.querySelector(".searchContent");
 let currentDayTemp = document.querySelector(".currentDayTemp");
@@ -22,6 +23,8 @@ let fewdata = document.querySelector(".fewdata");
 let uvFeedback = ["Moderate", "High", "Very High", "Extreme"]
 
 
+//Main logic starts here
+// sugesstions for the search area
 searchArea.addEventListener("input", () => {
     let p1 = new Promise((resolve, reject) => {
         let data1 = fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${searchArea.value}&count=10&language=en`)
@@ -47,75 +50,12 @@ searchArea.addEventListener("input", () => {
 
             // adding event listener to each search result
             itemElement.addEventListener('click', () => {
-                let p3 = new Promise((resolve, reject) => {
-                    let data3 = fetch(`https://air-quality-api.open-meteo.com/v1/air-quality?latitude=${(response.results)[i].latitude}&longitude=${(response.results)[i].longitude}&current=european_aqi,us_aqi,pm10,pm2_5&timezone=auto`)
-                    resolve(data3)
-
-                })
-                p3.then((val3) => {
-                    return new Promise((resolve, reject) => {
-                        let readableData3 = val3.json();
-                        resolve(readableData3)
-
-                    });
-
-                }).then((response3) => {
-                    // console.log(response2.daily.uv_index_max[0])
-                    let aqiNewVal = (response3.current.us_aqi) / 125
-                    console.log(aqiNewVal)
-
-
-                    aqiData.innerHTML = `<h6>AQI: ${response3.current.us_aqi}</h6>`
-
-
-                    // console.log(uvNewVal);
-                    if (aqiNewVal <= 1) {
-                        aqiData.classList.add("val1")
-
-                    }
-                    else if (aqiNewVal <= 2) {
-                        aqiData.classList.add("val2")
-
-                    }
-                    else if (aqiNewVal <= 3) {
-                        aqiData.classList.add("val3")
-
-                    }
-                    else {
-                        aqiData.classList.add("val4")
-
-                    }
-
-                    console.log(response3.current.us_aqi)
-                })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
                 searchContent.innerHTML = '';
                 // console.log((response.results)[i].name)
                 let p2 = new Promise((resolve, reject) => {
-
-
                     let data2 = fetch(`https://api.open-meteo.com/v1/forecast?latitude=${(response.results)[i].latitude}&longitude=${(response.results)[i].longitude}&current=temperature_2m&daily=temperature_2m_max,temperature_2m_min&&current=relative_humidity_2m,precipitation,rain,wind_speed_10m&daily=sunrise,sunset,daylight_duration,sunshine_duration&daily=uv_index_max,uv_index_clear_sky_max,precipitation_sum,rain_sum&timezone=auto`);
-
-                    let data3 = fetch(`https://air-quality-api.open-meteo.com/v1/air-quality?latitude=${(response.results)[i].latitude}&longitude=${(response.results)[i].longitude}&current=european_aqi,us_aqi,pm10,pm2_5&timezone=auto`)
-                    resolve(data2, data3)
+                    resolve(data2)
 
                 });
 
@@ -123,7 +63,7 @@ searchArea.addEventListener("input", () => {
                     return new Promise((resolve, reject) => {
                         let readableData2 = val2.json();
                         let readableData3 = val2.json();
-                        resolve(readableData2, readableData3)
+                        resolve(readableData2)
 
                     });
 
@@ -153,17 +93,14 @@ searchArea.addEventListener("input", () => {
                     }
 
 
-
-                    // console.log(response2)
-
-
-
                     manyClocks.innerHTML = '';
                     dayTime.push(new Date(`${response2.daily.sunrise[0]}`));
                     dayTime.push(new Date())
                     dayTime.push(new Date(`${response2.daily.sunset[0]}`));
                     // console.log(dayTime)
                     for (let k = 1; k < 4; k++) {
+
+                        // making all the clocks
                         let clock = document.createElement('div');
                         clock.classList.add("clock", `clock${k}`)
                         clock.innerHTML = `  <h3>${dayTimeKey[k - 1]}</h3>
@@ -172,13 +109,7 @@ searchArea.addEventListener("input", () => {
                         <div class="second second${k}"></div>`;
                         manyClocks.append(clock)
 
-
-
-                        if (k != 2) {
-
-                            htime = dayTime[k - 1].getHours();
-                            mtime = dayTime[k - 1].getMinutes();
-                            stime = dayTime[k - 1].getSeconds();
+                        function clockTime() {
 
                             hrotation = 30 * htime + mtime / 2;
                             mrotation = 6 * mtime;
@@ -191,7 +122,19 @@ searchArea.addEventListener("input", () => {
 
                             hour.style.transform = `rotate(${hrotation - 90}deg)`;
                             minute.style.transform = `rotate(${mrotation - 90}deg)`;
-                            second.style.transform = `rotate(${srotation - 90}deg)`;
+                            second.style.transform = `rotate(${srotation + 90}deg)`;
+                        }
+
+
+
+                        if (k != 2) {
+
+                            htime = dayTime[k - 1].getHours();
+                            mtime = dayTime[k - 1].getMinutes();
+                            stime = dayTime[k - 1].getSeconds();
+                            clockTime();
+
+
                         }
 
 
@@ -204,36 +147,21 @@ searchArea.addEventListener("input", () => {
                                 htime = d.getHours();
                                 mtime = d.getMinutes();
                                 stime = d.getSeconds();
+                                clockTime();
 
-                                hrotation = 30 * htime + mtime / 2;
-                                mrotation = 6 * mtime;
-                                srotation = 6 * stime;
-                                // console.log(htime, mtime, stime, hrotation)
 
-                                minute = document.querySelector(`.minute${k}`);
-                                second = document.querySelector(`.second${k}`);
-                                hour = document.querySelector(`.hour${k}`);
-
-                                hour.style.transform = `rotate(${hrotation - 90}deg)`;
-                                minute.style.transform = `rotate(${mrotation - 90}deg)`;
-                                second.style.transform = `rotate(${srotation - 90}deg)`;
                             }, 1000);
 
                         }
 
 
-
-
-
-
                     }
+                    // reset the dayTime array
+                    dayTime = [];
 
 
 
-
-
-
-
+                    // DOM manipulation
                     currentDayTemp.innerHTML = `${response2.current.temperature_2m}<sup class="superscript">&deg;C</sup>`
                     celVal = response2.current.temperature_2m;
                     farVal = 32 + (9 / 5) * celVal;
@@ -241,10 +169,11 @@ searchArea.addEventListener("input", () => {
                     cDay.innerText = `${String(new Date(`${(response2.current.time)}`)).slice(0, 3)}`
                     cDate.innerText = `${(response2.current.time).slice(8, 10)} ${month[Number.parseInt((response2.current.time).slice(5.7)) - 1]} '${(response2.current.time).slice(2, 4)}`;
                     areaName.innerText = (response.results)[i].name;
-                    nextDayDetails.innerHTML = '';
-                    weatherDetails.innerHTML = '';
 
 
+
+                    //current weather details showing for the searched region
+                    weatherDetails.innerHTML = '';  //first clear the previous data
                     weatherDetails.innerHTML = `  <div class="windSpeed"><img id="weatherIcon" alt=""
                             src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-512.png"> Wind: 
                         <span class="windData">${response2.current.wind_speed_10m} km/h</span>
@@ -254,13 +183,16 @@ searchArea.addEventListener("input", () => {
                             src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-512.png"> Humidity: 
                         <span class="humidityData">${response2.current.relative_humidity_2m} mm</span>
                     </div>
-
+                    
                     <div class="rain">|<img id="weatherIcon" alt=""
-                            src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-512.png"> Rain: 
-                        <span class="raindDta">${response2.current.rain} mm</span>
+                    src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-512.png"> Rain: 
+                    <span class="raindDta">${response2.current.rain} mm</span>
                     </div>`;
 
 
+
+                    // Future days weather details showing of the searchedd region.
+                    nextDayDetails.innerHTML = ''; // first clear the previous data
                     for (let j = 1; j < 6; j++) {
                         let nextDaysContainer = document.createElement('div');
                         nextDaysContainer.classList.add('nextDaysContainer');
@@ -279,6 +211,52 @@ searchArea.addEventListener("input", () => {
 
 
                 })
+
+
+                // fetching air quality index
+
+                let p3 = new Promise((resolve, reject) => {
+                    let data3 = fetch(`https://air-quality-api.open-meteo.com/v1/air-quality?latitude=${(response.results)[i].latitude}&longitude=${(response.results)[i].longitude}&current=european_aqi,us_aqi,pm10,pm2_5&timezone=auto`)
+                    resolve(data3)
+
+                })
+                p3.then((val3) => {
+                    return new Promise((resolve, reject) => {
+                        let readableData3 = val3.json();
+                        resolve(readableData3)
+
+                    });
+
+                }).then((response3) => {
+                    // console.log(response2.daily.uv_index_max[0])
+                    let aqiNewVal = (response3.current.us_aqi) / 125
+                    console.log(aqiNewVal)
+
+
+                    aqiData.innerHTML = `<h6>AQI: ${response3.current.us_aqi}</h6>`;
+
+
+                    // console.log(uvNewVal);
+                    if (aqiNewVal <= 1) {
+                        aqiData.classList.add("val1")
+
+                    }
+                    else if (aqiNewVal <= 2) {
+                        aqiData.classList.add("val2")
+
+                    }
+                    else if (aqiNewVal <= 3) {
+                        aqiData.classList.add("val3")
+
+                    }
+                    else {
+                        aqiData.classList.add("val4")
+
+                    }
+
+                    console.log(response3.current.us_aqi)
+                })
+
             })
 
 
@@ -288,6 +266,10 @@ searchArea.addEventListener("input", () => {
     })
 });
 
+
+
+
+// adding method to convert degree into farheneit and vice-versa
 cel.addEventListener('click', () => {
     cel.style.background = "rgba(255,255,0,0.6)"
     far.style.background = "";
